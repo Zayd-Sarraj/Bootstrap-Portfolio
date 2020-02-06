9b3b8ed18d2b70a83d1459005482ccd65dc39bb0
Zayd Sarraj <zayd.sarraj@gmail.com>
2020-02-06 04:41:08 -0500

    portfolio page completed along with simple template for future projects

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,3 +2,23 @@ $(".p-link").click(function (e) {
     e.preventDefault();
     window.open("portfolio.html")
 });
+
+$(".note-taker-link").click(function (e) { 
+    e.preventDefault();
+    window.open("https://sleepy-harbor-86048.herokuapp.com/")
+});
+
+$("#note-taker-repo").click(function (e) { 
+    e.preventDefault();
+    window.open("https://github.com/Zaydorade/Note-taker")
+});
+
+$(".teamprofile-link").click(function (e) { 
+    e.preventDefault();
+    window.open("https://zaydorade.github.io/Team-profile-generator/output/teamprofile.html")
+});
+
+$("#teamprofile-repo").click(function (e) { 
+    e.preventDefault();
+    window.open("https://github.com/Zaydorade/Team-profile-generator")
+});
